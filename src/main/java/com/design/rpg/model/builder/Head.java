8f94b980ab8bd9a619a61a015879fd92850962fe
liying <470549092@qq.com>
2019-03-31 02:10:24 +0800

    fix builder

--- src/main/java/com/design/rpg/model/builder/Head.java
+++ src/main/java/com/design/rpg/model/builder/Head.java
@@ -1,10 +1,16 @@
 package com.design.rpg.model.builder;
 
+import com.design.rpg.form.EquipmentType;
+
 /**
  * @author deng
  * @date 2019/03/16
  */
 public class Head extends Equipment{
+    public Head(){
+        super();
+        this.type= EquipmentType.HEAD;
 
+    }
 
 }
